5aedf560e4a8119e892005d929177e27d03e87c7
~venir <luca@venir.dev>
2025-10-31 09:52:04 +0100

    first exercise together

--- android/app/src/main/kotlin/com/example/corso_flutter_soluzione_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.corso_flutter_soluzione_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:corso_flutter_soluzione_1/main.dart';
+import 'package:color_changer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
