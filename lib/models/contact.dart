261469df13ba16ef6bff4d83144bcb8a45595757
Moustapha Ndiaye <moustapha.ndiaye@soluzione1.it>
2025-11-10 14:07:19 +0100

    [WIP] - contact list component

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,8 +1,28 @@
+import "package:flutter/material.dart";
+
 class Contact {
   Contact({required this.value, required this.type});
 
   String value;
   ContactType type;
+
+  IconData get icon {
+    switch (type) {
+      case ContactType.email:
+        return Icons.email;
+      case ContactType.phone:
+        return Icons.phone;
+    }
+  }
+
+  String get label {
+    switch (type) {
+      case ContactType.email:
+        return "Email: ";
+      case ContactType.phone:
+        return "Phone: ";
+    }
+  }
 }
 
 enum ContactType { email, phone }
--- lib/widgets/contact_list.dart
+++ lib/widgets/contact_list.dart
@@ -1,3 +1,6 @@
+import "dart:ffi";
+
+import "package:color_changer/models/contact.dart";
 import "package:color_changer/models/person.dart";
 import "package:flutter/material.dart";
 import "package:reactive_forms/reactive_forms.dart";
@@ -11,41 +14,115 @@ class ContactList extends StatefulWidget {
 
 class _ContactListState extends State<ContactList> {
   List<Person> people = [
-    Person(firstName: "Jack", lastName: "Brown", contacts: List.empty()),
+    Person(
+      firstName: "Jack",
+      lastName: "Brown",
+      contacts: [
+        Contact(value: "johnwhite@gmail.com", type: ContactType.email),
+        Contact(value: "+00 1234567879", type: ContactType.phone),
+      ],
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 36),
+      padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 36),
       child: Column(
-        spacing: 40,
+        spacing: 30,
         children: [
           Text(
             "Contact List",
             style: theme.textTheme.headlineMedium,
             textAlign: TextAlign.center,
           ),
-          ListView(
-            children: [
-              for (final person in people)
-                Card(
-                  child: Column(
-                    children: [
-                      Text(
-                        " ${person.firstName} ${person.lastName}",
-                        style: theme.textTheme.headlineMedium,
+          ElevatedButton.icon(
+            onPressed: openDialog,
+            label: const Text("Add New"),
+            icon: const Icon(Icons.add),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: theme.colorScheme.inversePrimary,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                for (final person in people)
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsetsGeometry.symmetric(
+                        vertical: 42,
+                        horizontal: 36,
                       ),
-                    ],
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 20,
+                        children: [
+                          Text(
+                            " ${person.firstName} ${person.lastName}",
+                            style: theme.textTheme.headlineMedium,
+                          ),
+                          for (final contact in person.contacts)
+                            Container(
+                              alignment: Alignment.center,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                spacing: 6,
+                                children: [
+                                  Text(
+                                    contact.label,
+                                    style: theme.textTheme.labelLarge,
+                                  ),
+                                  ElevatedButton.icon(
+                                    onPressed: contactPerson,
+                                    label: Text(
+                                      " ${contact.value}",
+                                      style: theme.textTheme.titleMedium,
+                                    ),
+                                    icon: Icon(contact.icon),
+                                  ),
+                                ],
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
     );
   }
+
+  Future<void> openDialog() async {
+    final form = await showDialog<Person>(
+      context: context,
+      builder: (context) {
+        return const ContactForm();
+      },
+    );
+
+    if (form == null) return;
+
+    final contacts = form.contacts as List<Contact?>;
+    final assertedContacts = contacts.map((v) => v!).toList();
+
+    setState(() {
+      people.add(
+        Person(
+          firstName: form.firstName,
+          lastName: form.lastName,
+          contacts: assertedContacts,
+        ),
+      );
+    });
+  }
+
+  void contactPerson() {}
+  void checkContactIcon(Contact contact) {}
 }
 
 class ContactForm extends StatefulWidget {
@@ -69,7 +146,14 @@ class _ContactFormState extends State<ContactForm> {
         Validators.minLength(2),
       ],
     ),
-    "contacts": FormArray<String>([]),
+    "email": FormControl<String>(
+      validators: [
+        Validators.email,
+      ],
+    ),
+    "phone": FormControl<Int>(
+      validators: [],
+    ),
   });
 
   FormArray<String> get contacts {
@@ -78,26 +162,63 @@ class _ContactFormState extends State<ContactForm> {
 
   @override
   Widget build(BuildContext context) {
+    final windowSize = MediaQuery.of(context).size;
+
     return Dialog(
-      child: ReactiveForm(
-        formGroup: form,
-        child: Column(
-          children: [
-            ReactiveTextField<String>(
-              formControlName: "firstName",
-              textInputAction: TextInputAction.next,
-            ),
-            ReactiveTextField<String>(
-              formControlName: "lastName",
-              textInputAction: TextInputAction.next,
-            ),
-            for (final contact in contacts.controls)
+      constraints: const BoxConstraints(),
+      child: Padding(
+        padding: const EdgeInsets.all(36),
+        child: ReactiveForm(
+          formGroup: form,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               ReactiveTextField<String>(
-                formControl: contact as FormControl<String>,
+                formControlName: "firstName",
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: "First Name",
+                  hintText: "enter here your first name",
+                  icon: Icon(Icons.person),
+                ),
               ),
-          ],
+              ReactiveTextField<String>(
+                formControlName: "lastName",
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: "Last Name",
+                  hintText: "enter here your last name",
+                  icon: Icon(Icons.person),
+                ),
+              ),
+              ReactiveTextField<String>(
+                formControlName: "email",
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  hintText: "enter here your email",
+                  icon: Icon(Icons.mail),
+                ),
+              ),
+              ReactiveTextField<String>(
+                formControlName: "phone",
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                  labelText: "Phone Number",
+                  hintText: "enter here your phone number",
+                  icon: Icon(Icons.phone),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void save() {
+    if (!form.valid) return;
+
+    Navigator.of(context).pop(form.value);
   }
 }
